d912df48b3c4074c74f910f6e597c31bf71e5924
JohnVS <johnbell417@gmail.com>
2018-04-21 17:16:12 -0400

    Added Memory Vulns
    0.0.1

---
+++ Memory/setmem/setmem.js
@@ -0,0 +1,53 @@
+function go() {
+var nex = RegExp.input = {toString: go_};
+var abuff;
+    abuff = new ArrayBuffer(0x18);
+var setarr32;
+    setarr32 = new Uint32Array(abuff);
+var set32 = setarr32;
+var setarr8;
+    setarr8 = new Uint8Array(abuff);
+var set8 = setarr8;
+var setbuff = prompt('Buffer to scan: ', '0x18');
+var buff;
+    buff = setbuff;
+for (let i in {xx: 0}) {
+    for (let j = 0; j < 0x18; j++) {
+        set8[0] = buff;
+        set8[1] = buff;
+        set8[2] = buff;
+        set8[3] = buff;
+        set8[4] = buff;
+        set8[5] = buff;
+        set8[6] = buff;
+        set8[7] = buff;
+        set8[8] = buff;
+        set8[9] = buff;
+        set8[10] = buff;
+        set32[0] = buff;
+        set32[1] = buff;
+        set32[2] = buff;
+        set32[3] = buff;
+        set32[4] = buff;
+        set32[5] = buff;
+        set32[6] = buff;
+        set32[7] = buff;
+        set32[8] = buff;
+        set32[9] = buff;
+        set32[10] = buff;
+        var kext;
+            kext += set8 + set32;
+}
+}
+eval(nex);
+alert(kext);
+}
+function go_() {
+setTimeout(function() {
+set8 = [];
+set32 = [];
+kext = null;
+location.reload();
+window.location.href += '#';
+}, 100);
+}
